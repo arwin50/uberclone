5260d6aaf5935fef543acb0c74a26c72df059bdb
powtzulist <delasanarwin@gmail.com>
2025-02-06 01:02:19 +0800

    folder routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,5 +27,11 @@ export default function RootLayout() {
     return null;
   }
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import React from "react";
+import { Redirect } from "expo-router";
 
-export default function Index() {
-  return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+const Home = () => {
+  return <Redirect href={"/(auth)/welcome"} />;
+};
+
+export default Home;
